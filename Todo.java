0acbe55cbd938917fa8ba455485fc7eba7542e11
Zandi Milligan <zmilligan@ou.edu>
2019-03-14 15:04:44 -0500

    completed enum tasks

--- Todo.java
+++ Todo.java
@@ -6,14 +6,38 @@ OU ID_______________: 112982309
 OU EMAIL____________: zmilligan@ou.edu
 
 Tasks:
-1. 
-2. 
+1. Create class headings
+2. Transfer javadoc info and skeleton methods
+3. GamePieceAppearance class
+4. Color enum
+5. Shape enum
+6. Location enum
+7. GamePiece enum
+8. 
+9. 
+10. 
 
 Estimated Time (minutes):
-1. 
-2. 
+1. 7
+2. 30
+3. 4
+4. 10
+5. 5
+6. 5
+7. 5
+8. 
+9. 
+10. 
 
 Actual Time (minutes):
-1. 
-2. 
+1. 7
+2. 55
+3. 4
+4. 7
+5. 3
+6. 2
+7. 6
+8. 
+9. 
+10. 
 */}
