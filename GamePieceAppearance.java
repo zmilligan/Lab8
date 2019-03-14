8ec8864182be3eff72d7ca20cc8c26301183c444
Zandi Milligan <zmilligan@ou.edu>
2019-03-14 13:09:58 -0500

    completed class and javadoc info

--- GamePieceAppearance.java
+++ GamePieceAppearance.java
@@ -1,5 +1,40 @@
-
+/**
+ * Class to represent the appearance of a game piece. That is, the color and shape of a game piece.
+ * 
+ * @author Stephen
+ * @version 2019-03-13
+ */
 public class GamePieceAppearance 
 {
+	private Color color; 
+	private Shape shape;
+	
+	/**
+	 * Stores the color and shape describing the appearance of the game piece.
+	 * @param color The game piece's color.
+	 * @param shape The game piece's shape.
+	 */
+	public GamePieceAppearance(Color color, Shape shape)
+	{
+		this.color = color;
+		this.shape = shape;
+	}
 
+	/**
+	 * Gets the color of the game piece.
+	 * @return The game piece's color.
+	 */
+	public Color getColor()
+	{
+		return color;
+	}
+	
+	/**
+	 * Gets the shape of the game piece.
+	 * @return The game piece's shape.
+	 */
+	public Shape getShape()
+	{
+		return shape;
+	}
 }
