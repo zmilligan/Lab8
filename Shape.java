a42bce2c92a3c720e54c06d5fd52356341eff65f
Zandi Milligan <zmilligan@ou.edu>
2019-03-14 14:04:11 -0500

    completed methods and javadoc

--- Shape.java
+++ Shape.java
@@ -1,4 +1,22 @@
-
-public enum Shape {
-
+/**
+ * Enum to represent the shape of a game piece.
+ * 
+ * @author Stephen
+ * @version 2019-03-13
+ *
+ */
+public enum Shape
+{
+	THIMBLE, BOOT, RACECAR;
+	
+	/**
+	 * Gives the name of the value of the enum object, in lowercase.
+	 * 
+	 * @return The enum object's name (either thimble, boot, racecar) in lowercase.
+	 */
+	@Override
+	public String toString()
+	{
+		return this.name().toLowerCase();
+	}
 }
