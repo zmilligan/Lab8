0c610118cd4a8355ed9a661e03dbd360a7d6b782
Zandi Milligan <zmilligan@ou.edu>
2019-03-14 14:08:00 -0500

    completed javadoc and location enums

--- Location.java
+++ Location.java
@@ -1,4 +1,10 @@
-
-public enum Location {
-
+/**
+ * Enum to represent a location within a house in a board game.
+ * 
+ * @author Stephen
+ * @version 2019-03-13
+ */
+public enum Location
+{
+	KITCHEN, CONSERVATORY, DINING_ROOM, BALLROOM, STUDY, HALL, LOUNGE, LIBRARY, BILLIARD_ROOM;
 }
