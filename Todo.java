996f87decebcff377606dfa961ce0b1ad3a0b5ee
Zandi Milligan <zmilligan@ou.edu>
2019-03-14 12:59:46 -0500

    class headings

---
+++ Todo.java
@@ -0,0 +1,19 @@
+
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-03-14
+OU ID_______________: 112982309
+OU EMAIL____________: zmilligan@ou.edu
+
+Tasks:
+1. 
+2. 
+
+Estimated Time (minutes):
+1. 
+2. 
+
+Actual Time (minutes):
+1. 
+2. 
+*/}
