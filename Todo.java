6ec91d0c1c4fea475f63a7d7dd11437ffd35922e
Zandi Milligan <zmilligan@ou.edu>
2019-03-19 13:58:57 -0500

    updated driver tasks

--- Todo.java
+++ Todo.java
@@ -19,6 +19,11 @@ Tasks:
 11. BG get methods return ArrayLists
 12. BG get methods return Sets
 13. BG get individual methods
+14. Color value tests
+15. Location names tests
+16. Shape tests
+17. GamePiece tests - by extension, also covers GamePieceAppearance class
+18. BoardGame tests
 
 Estimated Time (minutes):
 1. 7
@@ -34,6 +39,11 @@ Estimated Time (minutes):
 11. 10
 12. 15
 13. 7
+14. 5
+15. 5
+16. 6
+17. 20
+18. 30
 
 Actual Time (minutes):
 1. 7
@@ -49,4 +59,9 @@ Actual Time (minutes):
 11. 12
 12. 7
 13. 11
+14. 6
+15. 
+16. 
+17. 
+18. 
 */}
