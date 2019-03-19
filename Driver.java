22d5f2f33b3f489cfb619597ad9e1e308743626f
Zandi Milligan <zmilligan@ou.edu>
2019-03-19 13:59:24 -0500

    Color enum tests

--- Driver.java
+++ Driver.java
@@ -1,4 +1,57 @@
+import org.junit.Test;
+import org.junit.Assert;
 
-public class Driver {
-
+/**
+ * Driver to test the BoardGame and associated classes and Enums.
+ * 
+ * @author Zandi Milligan
+ * @version 2019-03-18
+ *
+ */
+public class Driver
+{
+	/**
+	 * Tests for the Color Enum
+	 */
+	@Test
+	public void testColorValues()
+	{
+	Color col = null;
+	// RED:
+	col = Color.RED;
+	Assert.assertEquals("Incorrect r value in color " + col.name(), 255, col.getR());
+	Assert.assertEquals("Incorrect g value in color " + col.name(), 0, col.getG());
+	Assert.assertEquals("Incorrect b value in color " + col.name(), 0, col.getB());
+	
+	// GREEN:
+	col = Color.GREEN;
+	Assert.assertEquals("Incorrect r value in color " + col.name(), 0, col.getR());
+	Assert.assertEquals("Incorrect g value in color " + col.name(), 255, col.getG());
+	Assert.assertEquals("Incorrect b value in color " + col.name(), 0, col.getB());
+	
+	//BLUE:
+	col = Color.BLUE;
+	Assert.assertEquals("Incorrect r value in color " + col.name(), 0, col.getR());
+	Assert.assertEquals("Incorrect g value in color " + col.name(), 0, col.getG());
+	Assert.assertEquals("Incorrect b value in color " + col.name(), 255, col.getB());
+	
+	//YELLOW:
+	col = Color.YELLOW;
+	Assert.assertEquals("Incorrect r value in color " + col.name(), 255, col.getR());
+	Assert.assertEquals("Incorrect g value in color " + col.name(), 255, col.getG());
+	Assert.assertEquals("Incorrect b value in color " + col.name(), 0, col.getB());
+	
+	//CYAN:
+	col = Color.CYAN;
+	Assert.assertEquals("Incorrect r value in color " + col.name(), 0, col.getR());
+	Assert.assertEquals("Incorrect g value in color " + col.name(), 255, col.getG());
+	Assert.assertEquals("Incorrect b value in color " + col.name(), 255, col.getB());
+	
+	//MAGENTA:
+	col = Color.MAGENTA;
+	Assert.assertEquals("Incorrect r value in color " + col.name(), 255, col.getR());
+	Assert.assertEquals("Incorrect g value in color " + col.name(), 0, col.getG());
+	Assert.assertEquals("Incorrect b value in color " + col.name(), 255, col.getB());
+	}
+	
 }
