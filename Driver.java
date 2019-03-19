d46f1542d5a13693c7ceb0605f1214856d75a37d
Zandi Milligan <zmilligan@ou.edu>
2019-03-19 14:11:40 -0500

    Location enum tests

--- Driver.java
+++ Driver.java
@@ -54,4 +54,20 @@ public class Driver
 	Assert.assertEquals("Incorrect b value in color " + col.name(), 255, col.getB());
 	}
 	
+	/**
+	 * Tests for the Location Enum
+	 */
+	@Test
+	public void testLocation()
+	{
+	Assert.assertEquals("Location enum value incorrect.", Location.HALL, Location.valueOf("HALL"));
+	Assert.assertEquals("Location enum value incorrect.", Location.LOUNGE, Location.valueOf("LOUNGE"));
+	Assert.assertEquals("Location enum value incorrect.", Location.DINING_ROOM, Location.valueOf("DINING_ROOM"));
+	Assert.assertEquals("Location enum value incorrect.", Location.KITCHEN, Location.valueOf("KITCHEN"));
+	Assert.assertEquals("Location enum value incorrect.", Location.BALLROOM, Location.valueOf("BALLROOM"));
+	Assert.assertEquals("Location enum value incorrect.", Location.CONSERVATORY, Location.valueOf("CONSERVATORY"));
+	Assert.assertEquals("Location enum value incorrect.", Location.BILLIARD_ROOM, Location.valueOf("BILLIARD_ROOM"));
+	Assert.assertEquals("Location enum value incorrect.", Location.LIBRARY, Location.valueOf("LIBRARY"));
+	Assert.assertEquals("Location enum value incorrect.", Location.STUDY, Location.valueOf("STUDY"));
+	}
 }
