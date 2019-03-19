fb56783914fcacaaac8eb59eaf3e9166439f7ae6
Zandi Milligan <zmilligan@ou.edu>
2019-03-19 15:43:39 -0500

    get methods returning Arrays and ArrayLists

--- Driver.java
+++ Driver.java
@@ -1,4 +1,7 @@
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import org.junit.Assert;
 
 /**
@@ -141,7 +144,49 @@ public class Driver
 		Assert.assertEquals("BoardGame getPlayerWithGamePiece incorrect", "Megan", first.getPlayerWithGamePiece(GamePiece.RED_THIMBLE));
 		Assert.assertEquals("BoardGame getPlayerWithGamePiece incorrect", null, first.getPlayerWithGamePiece(GamePiece.YELLOW_BOOT));
 		
-	}
+		//getPlayersLocation methods
+		Assert.assertEquals("BoardGame getPlayersLocation incorrect", null, first.getPlayersLocation("David"));
+		Assert.assertEquals("BoardGame getPlayersLocation incorrect", Location.DINING_ROOM, first.getPlayersLocation("Zandi"));
+		
+		//movePlayer method
+		first.movePlayer("Zandi", Location.KITCHEN);
+		Assert.assertEquals("BoardGame movePlayer incorrect", Location.KITCHEN, first.getPlayersLocation("Zandi"));
+		
+		//moveTwoPlayers method
+		Assert.assertEquals("BoardGame moveTwoPlayers return incorrect", "Zandi",
+				first.moveTwoPlayers(new String[] {"Megan", "Zandi"}, new Location[] {Location.STUDY, Location.LIBRARY})[0]);
+		Assert.assertEquals("BoardGame moveTwoPlayers move incorrect", Location.STUDY, first.getPlayersLocation("Megan"));
+		Assert.assertEquals("BoardGame moveTwoPlayers move incorrect", Location.LIBRARY, first.getPlayersLocation("Zandi"));
+		//switch the order of players to ensure proper running of moveTwoPlayers
+		Assert.assertEquals("BoardGame moveTwoPlayers return incorrect", "Megan",
+				first.moveTwoPlayers(new String[] {"Zandi", "Megan"}, new Location[] {Location.KITCHEN, Location.KITCHEN})[1]);
+		Assert.assertEquals("BoardGame moveTwoPlayers move incorrect", Location.KITCHEN, first.getPlayersLocation("Zandi"));
+		Assert.assertEquals("BoardGame moveTwoPlayers move incorrect", Location.KITCHEN, first.getPlayersLocation("Megan"));
+		
+		//getPlayersAtLocation method
+		ArrayList<String> inKitchen1 = first.getPlayersAtLocation(Location.KITCHEN);
+		ArrayList<String> inHall = first.getPlayersAtLocation(Location.HALL);
+		ArrayList<String> inLibrary = first.getPlayersAtLocation(Location.LIBRARY);
+		Assert.assertEquals("BoardGame getPlayersAtLocation incorrect", true, inKitchen1.contains("Zandi"));
+		Assert.assertEquals("BoardGame getPlayersAtLocation incorrect", true, inKitchen1.contains("Megan"));
+		Assert.assertEquals("BoardGame getPlayersAtLocation incorrect", false, inKitchen1.contains("David"));
+		Assert.assertEquals("BoardGame getPlayersAtLocation incorrect", true, inHall.isEmpty());
+		Assert.assertEquals("BoardGame getPlayersAtLocation incorrect", true, inLibrary.isEmpty());
+		
+		//getGamePiecesAtLocation method
+		ArrayList<GamePiece> inKitchen2 = first.getGamePiecesAtLocation(Location.KITCHEN);
+		ArrayList<GamePiece> inBallroom = first.getGamePiecesAtLocation(Location.BALLROOM);
+		ArrayList<GamePiece> inConservatory = first.getGamePiecesAtLocation(Location.CONSERVATORY);
+		Assert.assertEquals("BoardGame getGamePiecesAtLocation incorrect", true, inKitchen2.contains(GamePiece.BLUE_BOOT));
+		Assert.assertEquals("BoardGame getGamePiecesAtLocation incorrect", true, inKitchen2.contains(GamePiece.RED_THIMBLE));
+		Assert.assertEquals("BoardGame getGamePiecesAtLocation incorrect", false, inKitchen2.contains(GamePiece.MAGENTA_RACER));
+		Assert.assertEquals("BoardGame getGamePiecesAtLocation incorrect", true, inBallroom.isEmpty());
+		Assert.assertEquals("BoardGame getGamePiecesAtLocation incorrect", true, inConservatory.isEmpty());
+		
+		//getPlayers method
+		
+		
+		}
 	
 	
 	
